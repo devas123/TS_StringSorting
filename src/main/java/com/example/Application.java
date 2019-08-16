ada0414ea654864bb22d8f4c0a2c8b6dfa7523d8
ggrigori <grigoriy.grigoriev@t-systems.com>
2019-08-16 17:09:40 +0300

    fixme

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -18,7 +18,5 @@ public class Application {
 	public static void main(String[] args) {
 		StringSorting stringSorting = new StringSorting(STR);
 		stringSorting.printResult();
-//		stringSorting.printResult();
-//		stringSorting.printResult();
 	}
 }
